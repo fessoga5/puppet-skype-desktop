62fd3b9f4a87c9cbb96f82133f09c3b7250fdb9d
root <root@d1stkfactory>
2014-03-22 07:55:41 +0000

    create module for install skype on debian whezzy

---
+++ manifests/init.pp
@@ -0,0 +1,25 @@
+# vim: sts=4 ts=4 sw=4 expandtab autoindent
+#
+#INSTALL SKYPE ON DESKTOP
+#
+class puppet-skype-desktop ($source, $destination = "/root/skype.deb") {
+    #support multiarch
+    exec{ "dpkg --add-architecture i386;apt-get update;":
+        path => '/usr/bin:/bin:/usr/sbin:/sbin',
+        unless => '/usr/bin/dpkg --print-foreign-architectures | /bin/grep i386'
+    }
+
+    wget::fetch { 'skype.deb':
+        source      => $source,
+        destination => $destination,
+        timeout     => 0,
+        verbose     => false,
+    }
+
+    package {"skype":
+        provider => dpkg,
+        ensure   => latest,
+        source   => $destination,
+        require  => Wget::Fetch['skype.deb']
+    }
+}

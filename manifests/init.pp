9f8f2271e00cc09e752f0c7a2358ae295c9aeca5
root <root@d1stkfactory>
2014-03-25 04:55:12 +0000

    fix redaclare

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,9 +4,12 @@
 #
 class puppet-skype-desktop ($source, $destination = "/root/skype.deb") {
     #support multiarch
-    exec{ "dpkg --add-architecture i386;apt-get update;":
-        path => '/usr/bin:/bin:/usr/sbin:/sbin',
-        unless => '/usr/bin/dpkg --print-foreign-architectures | /bin/grep i386'
+    if defined (Exec["i386"]) {
+        exec{ "i386":
+            command => "dpkg --add-architecture i386;apt-get update;",
+            path => '/usr/bin:/bin:/usr/sbin:/sbin',
+            unless => '/usr/bin/dpkg --print-foreign-architectures | /bin/grep i386'
+        }
     }
 
     wget::fetch { 'skype.deb':
